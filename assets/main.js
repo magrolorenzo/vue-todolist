53a371811c5636d5f453fe46908d46a3264217d3
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-11 12:38:37 +0100

    Aggiunto bonus, svuotamento variabile con add

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,7 @@
 // Definire una lista di todo e stamparli in pagina in un elenco
-
 //Predisporre un input testuale per aggiungere un nuovo todo:
 // al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
 // al click su un item della lista, il todo viene rimosso
-
 // BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
 var app = new Vue({
     el: "#root",
@@ -20,16 +18,15 @@ var app = new Vue({
             "Fare benzina",
             "Fare esercizio"
         ]
-
     },
 
     methods: {
         add(){
             if(this.todo != ""){
                 this.todos.push(this.todo);
+                this.todo="";
             }
         },
-
         remove(position){
             // Rimuovere l'elemento dell array corrispondente alla posizione index
             this.todos.splice(position, 1);
